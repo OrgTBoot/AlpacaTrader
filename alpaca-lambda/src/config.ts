116b6dc38b414f62aabf4d658e2e08d54ce6af1f
Vlad Munteanu <munteanu.vladislav89@gmail.com>
2023-02-23 23:31:03 -0800

    initial commit

---
+++ alpaca-lambda/src/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+    orderSize: 3, // % from byuing power
+    orderType: 'market', // limit, market
+    stopLoss: 3, // % from order price
+    extendedHours: false, // true | false
+};
+
+export const credentials = {
+    key: 'PKV8LUS6ALJ9S3RSMU0I',
+    secret: 'WyuBEVZcaoktIXCgBd1mmWteMrtYmm7EZyr3KZNH',
+    paper: true,
+};
---
+++ alpaca-lambda/src/interfaces/trade-signal.ts
@@ -0,0 +1,8 @@
+export interface TradeSignal {
+    ticker: string;
+    action: string;
+    sentiment: string;
+    quantity: string;
+    price: string;
+    time: string;
+}

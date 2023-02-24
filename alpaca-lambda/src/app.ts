97a05d2b5543dbcda6db085908c1a3aef5222c5f
Vlad Munteanu <munteanu.vladislav89@gmail.com>
2023-02-23 23:36:02 -0800

    added missing code

--- alpaca-lambda/src/app.ts
+++ alpaca-lambda/src/app.ts
@@ -13,23 +13,5 @@ import { AplacaService } from './aplaca_service';
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const aplacaService = new AplacaService();
 
-    // let response: APIGatewayProxyResult;
-    // try {
-    //     response = {
-    //         statusCode: 200,
-    //         body: JSON.stringify({
-    //             message: 'hello world',
-    //         }),
-    //     };
-    // } catch (err: unknown) {
-    //     console.log(err);
-    //     response = {
-    //         statusCode: 500,
-    //         body: JSON.stringify({
-    //             message: err instanceof Error ? err.message : 'some error happened',
-    //         }),
-    //     };
-    // }
-
     return aplacaService.processEvent(event);
 };
